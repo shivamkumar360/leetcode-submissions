8c9a19e581154f48aacb6dc2d7ae581b285d6543
shivamkumar360 <shivamankuri123@gmail.com>
2026-06-27 14:38:00 +0530

    Time: 0 ms (100%), Space: 27.5 MB (69.79%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -5,19 +5,19 @@ public:
     vector<int>xd={0,0,1,-1};
     vector<int>yd={1,-1,0,0};
 
-    void dfs(vector<vector<int>>&grid,int row,int col,vector<vector<int>>&vis)
+    void dfs(vector<vector<int>>&grid,int row,int col)
     {
         int n=grid.size();
         int m=grid[0].size();
         cnt++;
-        vis[row][col]=1;
+        grid[row][col]=0;
         for(int i=0;i<4;i++)
         {
             int xc=row+xd[i];
             int yc=col+yd[i];
-            if(xc>=0 && xc<n && yc>=0 && yc<m && grid[xc][yc] && !vis[xc][yc])
+            if(xc>=0 && xc<n && yc>=0 && yc<m && grid[xc][yc])
             {
-                dfs(grid,xc,yc,vis);
+                dfs(grid,xc,yc);
             }
         }
 
@@ -29,15 +29,14 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         int ans=0;
-        vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j] && grid[i][j])
+                if(grid[i][j])
                 {
                     cnt=0;
-                    dfs(grid,i,j,vis);
+                    dfs(grid,i,j);
                     ans=max(ans,cnt);
                 }
             }
