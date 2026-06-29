75d14dba280d46fe017a56945dc0e55d83c389a1
shivamkumar360 <shivamankuri123@gmail.com>
2026-06-29 21:25:04 +0530

    Time: 0 ms (100%), Space: 10.7 MB (35.49%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    const static int nmax=(int)1e5+2;
-    int bits[nmax];
     
-    int highestPowerOf2(int n) {
-    if (n == 0) return 0;
-    return 1 << (31 - __builtin_clz(n));
-}
-    void init()
+
+    void init(vector<int>& bits,int n)
     {
         bits[0]=0;
-    bits[1]=1;
-    bits[2]=1;
-    bits[3]=2;
-    bits[4]=1;
-        for(int i=5;i<nmax;i++)
+        int high = 1;
+    
+        for(int i=1;i<=n;i++)
         {
-            int high=highestPowerOf2(i);
-            int diff=i-high;
-            bits[i]=bits[diff]+1;
+            if(high*2==i)
+            {
+                high=i;
+            }
+            bits[i]=bits[i-high]+1;
         }
     }
     vector<int> countBits(int n) {
-        init();
-        vector<int>ans;
-        for(int i=0;i<=n;i++)
-        {
-            ans.push_back(bits[i]);
-        }
-        return ans;
+        vector<int> bits(n+1);
+        init(bits,n);
+        return bits;
         
         
     }
