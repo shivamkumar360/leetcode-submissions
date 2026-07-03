68408086875d425fb4025c61a4804d26fb16fa1e
shivamkumar360 <shivamankuri123@gmail.com>
2026-07-03 19:14:07 +0530

    Time: 15 ms (72.93%), Space: 87.9 MB (42.32%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -6,25 +6,27 @@ public:
         vector<long long >dp(n+1,-1e18);
         dp[0]=nums[0];
         deque<int>dq;
+        dq.push_back(0);
         for(int i=1;i<n;i++)
         {
             int newi=i-1;
-            if(newi>=0)
-            {
-            while(!dq.empty() && dp[newi]>=dp[dq.back()])
-            {
-                dq.pop_back();
-            }
-            dq.push_back(newi);
-            }
-            if(!dq.empty() && dq.front()<newi-k+1)
+            while(!dq.empty() && dq.front()<i - k)
             {
                 dq.pop_front();
             }
+             
             if(!dq.empty())
             {
                 dp[i]=nums[i]+dp[dq.front()];
             }
+          
+            while(!dq.empty() && dp[i]>=dp[dq.back()])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            
+           
         }
 
         return dp[n-1];
