f9fb5ac5b0383a38f6af66751debf9cc22f6a789
shivamkumar360 <shivamankuri123@gmail.com>
2026-07-03 23:03:06 +0530

    Time: 0 ms (100%), Space: 25.5 MB (30.09%) - LeetHub

--- 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
+++ 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
@@ -25,8 +25,24 @@ public:
 
     int minimumCoins(vector<int>& prices) {
         int n=prices.size();
-        vector<long long>dp(n,-1);
-        return fruit(0,prices,dp);
+        vector<long long>dp(n+1, 1e18);
+        deque<int>dq;
+        dp[n]=0;
+        dq.push_back(n);
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!dq.empty() && dq.front()>2*i+2)
+            {
+                dq.pop_front();
+            }
+            dp[i]=prices[i]+dp[dq.front()];
+            while(!dq.empty() && dp[dq.back()]>=dp[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        return dp[0];
 
 
         
