4047da70786bb778f561c8883844a898c8d7d8b1
shivamkumar360 <shivamankuri123@gmail.com>
2026-07-04 11:43:00 +0530

    Time: 0 ms (100%), Space: 25.2 MB (36.88%) - LeetHub

--- 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
+++ 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
-
-    long long fruit(int idx,vector<int>&prices,vector<long long>&dp)
-    {
-        int n=prices.size();
-        if(idx>=prices.size())
-        {
-            return 0;
-        }
-        if(dp[idx]!=-1)
-        {
-            return dp[idx];
-        }
-        long long ans=1e18;
-        for(int i=idx+1;i<=min(n,idx+idx+2);i++)
-        {
-            ans=min(ans,fruit(i,prices,dp));
-        }
-        return dp[idx]=prices[idx]+ans;
-    }
-
-
-
-
     int minimumCoins(vector<int>& prices) {
+
         int n=prices.size();
-        vector<long long>dp(n+1, 1e18);
-        deque<int>dq;
+        vector<long long>dp(n+1,1e18);
         dp[n]=0;
+        deque<int>dq;
         dq.push_back(n);
         for(int i=n-1;i>=0;i--)
         {
-            while(!dq.empty() && dq.front()>2*i+2)
+            while(!dq.empty() && dq.front()>min(n,2*i+2))
             {
                 dq.pop_front();
             }
@@ -43,8 +21,6 @@ public:
             dq.push_back(i);
         }
         return dp[0];
-
-
         
     }
 };
