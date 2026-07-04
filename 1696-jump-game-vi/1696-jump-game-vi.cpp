aec674850a6c7f623b7222cfc7896bdf5eeb3373
shivamkumar360 <shivamankuri123@gmail.com>
2026-07-04 11:12:02 +0530

    Time: 13 ms (78.57%), Space: 88 MB (42.05%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -3,33 +3,29 @@ public:
     int maxResult(vector<int>& nums, int k) {
 
         int n=nums.size();
-        vector<long long >dp(n+1,-1e18);
-        dp[0]=nums[0];
+        vector<long long>dp(n,-1e18);
         deque<int>dq;
+        dp[0]=nums[0];
         dq.push_back(0);
         for(int i=1;i<n;i++)
         {
-            int newi=i-1;
-            while(!dq.empty() && dq.front()<i - k)
+            while(!dq.empty() && dq.front()<i-k)
             {
                 dq.pop_front();
             }
-             
-            if(!dq.empty())
-            {
-                dp[i]=nums[i]+dp[dq.front()];
-            }
-          
-            while(!dq.empty() && dp[i]>=dp[dq.back()])
+            dp[i]=nums[i]+dp[dq.front()];
+            while(!dq.empty() && dp[dq.back()]<=dp[i])
             {
                 dq.pop_back();
             }
             dq.push_back(i);
-            
-           
-        }
 
+        }
         return dp[n-1];
+
+
+
+        
 
 
         
