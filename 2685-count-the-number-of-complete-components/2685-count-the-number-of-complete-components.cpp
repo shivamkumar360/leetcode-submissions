3d9b395e1f63c9f5186f28c98d14937c229b5ac3
shivamkumar360 <shivamankuri123@gmail.com>
2026-07-11 13:22:18 +0530

    Time: 60 ms (37.94%), Space: 132.6 MB (51.66%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -5,22 +5,16 @@ public:
     int nodes=0;
     int ed=0;
 
-    void dfs(int node,vector<pair<int,int>>adj[],vector<int>&edgevis,vector<int>&vis)
+    void dfs(int node,vector<int>adj[],vector<int>&vis)
     {
         vis[node]=1;
         nodes++;
+        ed+=adj[node].size();
         for(auto it:adj[node])
         {
-            int adjnode=it.first;
-            int edgenum=it.second;
-            if(!edgevis[edgenum])
+            if(!vis[it])
             {
-                edgevis[edgenum]=1;
-                ed++;
-            }
-            if(!vis[adjnode])
-            {
-                dfs(adjnode,adj,edgevis,vis);
+                dfs(it,adj,vis);
             }
         }
     }
@@ -30,18 +24,15 @@ public:
 
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
 
-        vector<pair<int,int>>adj[n];
-        int cnt=0;
+        vector<int>adj[n];
         for(auto it:edges)
         {
             int u=it[0];
             int v=it[1];
-            adj[u].push_back({v,cnt});
-            adj[v].push_back({u,cnt});
-            cnt++;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
         vector<int>vis(n,0);
-        vector<int>edgevis(edges.size(),0);
         int ans=0;
         for(int i=0;i<n;i++)
         {
@@ -49,8 +40,8 @@ public:
             {
                 nodes=0;
                 ed=0;
-                dfs(i,adj,edgevis,vis);
-                if(ed==(nodes*(nodes-1))/2)
+                dfs(i,adj,vis);
+                if(ed==(nodes*(nodes-1)))
                 {
                     ans++;
                 }
