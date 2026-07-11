43f1865531cfd9bab529d8d26129b0fd44457f4c
shivamkumar360 <shivamankuri123@gmail.com>
2026-07-11 12:49:39 +0530

    Time: 546 ms (5.18%), Space: 72.8 MB (5.48%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+   TreeNode* ans=NULL;
+
+
+    void lca(TreeNode* root,TreeNode* p,TreeNode*q,map<TreeNode*,pair<bool,bool>>&mp)
+    {
+        mp[root]={false,false};
+        if(root==p)
+        {
+            mp[root].first=true;
+        }
+        if(root==q)
+        {
+            mp[root].second=true;
+        }
+        if(root->left!=NULL)
+        {
+            lca(root->left,p,q,mp);
+            if(mp[root->left].first==true)
+            {
+                mp[root].first=true;
+            }
+            if(mp[root->left].second==true)
+            {
+                mp[root].second=true;
+            }
+        }
+        if(root->right!=NULL)
+        {
+            lca(root->right,p,q,mp);
+            if(mp[root->right].first==true)
+            {
+                mp[root].first=true;
+            }
+            if(mp[root->right].second==true)
+            {
+                mp[root].second=true;
+            }
+        }
+        if(mp[root].first==true && mp[root].second==true && ans==NULL)
+        {
+            ans= root;
+        }
+    }
+
+
+
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        map<TreeNode*,pair<bool,bool>>mp;
+        lca(root,p,q,mp);
+        return ans;
+
+
+
+        
+    }
+};
