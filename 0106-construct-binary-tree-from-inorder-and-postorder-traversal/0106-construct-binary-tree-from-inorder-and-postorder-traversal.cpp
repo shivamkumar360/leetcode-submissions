1ea4c8d1fb8e13ee634240b32b6c5aaa142ccb6d
shivamkumar360 <shivamankuri123@gmail.com>
2026-08-02 18:26:17 +0530

    Time: 7 ms (24.48%), Space: 32.4 MB (5.5%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -13,30 +13,21 @@ class Solution {
 public:
 
 
-    TreeNode* build(TreeNode* root,vector<int>&inorder,vector<int>&postorder,int st1,int en1,int st2,int en2)
+    TreeNode* build(TreeNode* root,vector<int>&inorder,vector<int>&postorder,int st1,int en1,int st2,int en2,vector<int>&pos)
     {
         root->val=postorder[en2];
-        int pos=-1;
-        for(int idx=st1;idx<=en1;idx++)
-        {
-            if(inorder[idx]==root->val)
-            {
-                pos=idx;
-                break;
-
-            }
-        }
-        int left=pos-st1;
-        int right=en1-pos;
+        int poss=pos[root->val+3000];
+        int left=poss-st1;
+        int right=en1-poss;
         if(left>0)
         {
             root->left=new TreeNode();
-            build(root->left,inorder,postorder,st1,pos-1,st2,en2-right-1);
+            build(root->left,inorder,postorder,st1,poss-1,st2,en2-right-1,pos);
         }
         if(right>0)
         {
             root->right=new TreeNode();
-            build(root->right,inorder,postorder,pos+1,en1,en2-right,en2-1);
+            build(root->right,inorder,postorder,poss+1,en1,en2-right,en2-1,pos);
         }
         return root;
 
@@ -50,8 +41,17 @@ public:
         if(inorder.empty())return NULL;
 
         int n=inorder.size();
+
+        vector<int> pos(6001);
+
+    for (int i = 0; i < inorder.size(); i++)
+    pos[inorder[i] + 3000] = i;
+
+
+
+
         TreeNode* root=new TreeNode();
-        return build(root,inorder,postorder,0,n-1,0,n-1);
+        return build(root,inorder,postorder,0,n-1,0,n-1,pos);
 
         
         
