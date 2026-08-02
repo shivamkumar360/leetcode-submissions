b90715676a5949393ba77057e17fd5793e06063b
shivamkumar360 <shivamankuri123@gmail.com>
2026-08-02 21:35:49 +0530

    Time: 20 ms (79.44%), Space: 23.5 MB (66.8%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,24 +14,44 @@ public:
 
     TreeNode* lca(TreeNode* root,TreeNode* p, TreeNode* q)
     {
-        if(root==NULL || root==p || root==q)
+
+        int pval=p->val;
+        int qval=q->val;
+        int rval=root->val;
+        if(pval>rval && qval>rval)
+        {
+            return lca(root->right,p,q);
+        }
+        else if(pval<rval && qval<rval)
+        {
+            return lca(root->left,p,q);
+        }
+        else 
         {
             return root;
         }
-        TreeNode* left=lca(root->left,p,q);
-        TreeNode* right=lca(root->right,p,q);
-        if(left!=NULL && right!=NULL)
-        {
-            return root;
-        }
-        else if(left==NULL)
-        {
-            return right;
-        }
-        else
-        {
-            return left;
-        }
+       
+
+
+
+        // if(root==NULL || root==p || root==q)
+        // {
+        //     return root;
+        // }
+        // TreeNode* left=lca(root->left,p,q);
+        // TreeNode* right=lca(root->right,p,q);
+        // if(left!=NULL && right!=NULL)
+        // {
+        //     return root;
+        // }
+        // else if(left==NULL)
+        // {
+        //     return right;
+        // }
+        // else
+        // {
+        //     return left;
+        // }
     }
 
 
